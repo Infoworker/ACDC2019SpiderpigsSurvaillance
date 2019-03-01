97eb1b070a176c14fcc6665453f2529cc02cf90e
silmer <dahl.silmer@gmail.com>
2019-03-01 23:41:23 +0100

    Tmp update

--- src/component/container/MapContainer.tsx
+++ src/component/container/MapContainer.tsx
@@ -3,7 +3,8 @@ import Map from '../Map/Map';
 import {compose} from 'recompose';
 import {inject,observer} from 'mobx-react';
 import * as image from '../../image/duff.png';
-import {IBuildningStoreProps,IPlayerStoreProps} from '../../store';
+import {IBuildningStoreProps,IPlayerStoreProps, IPlayer} from '../../store';
+import { timingSafeEqual } from 'crypto';
 
 
 interface IMapContainerProps{
@@ -13,6 +14,9 @@ interface IMapContainerProps{
 
 interface IMapContainerState{
     markers: any[];
+    playerMarkers: any[];
+    tick: number;
+    map: any;
 }
 
 
@@ -21,11 +25,62 @@ class MapContainer extends React.Component<IMapContainerProps,IMapContainerState
     constructor(props: IMapContainerProps) {
         super(props)
         this.state= {
-            markers: []
+            markers: [],
+            playerMarkers: [],
+            tick: Date.now(),
+            map: undefined
         }
+        //setInterval(this.tick, 2000);
+        setInterval(()=>{
+
+            if (this.state.playerMarkers && this.state.map) {
+                //console.log(this.state.playerMarkers);
+
+                this.state.playerMarkers.forEach(playerMarker=>{
+                    this.moveBus(this.state.map, playerMarker.marker, playerMarker.player);
+                })
+    
+                //this.moveBus(this.state.map, this.state.playerMarkers[0].marker, this.state.playerMarkers[0].player);
+
+                    // console.log(this.state.tick);
+                    // //console.log(Date.now());
+                    // if (Date.now()-this.state.tick > 2000) {
+                    //     //console.log(this.state.tick);
+                    //     //this.setState({tick: Date.now()});
+                        
+                    // } 
+                    console.log(Date.now());
+            }
+
+            this.setState({tick: Date.now()}, ()=> {
+                //console.log(this.state.tick);
+            });
+        }, 2000);
     }
 
+    
+    // tick() {
+    //     this.setState({tick: Date.now()}, ()=> {
+    //         console.log(this.state.tick);
+    //     });
+    // }
+
+    //componentDidUpdate
+
     render(){
+
+        // if (this.state.playerMarkers && this.state.map) {
+        //     //console.log(this.state.playerMarkers);
+
+        //         console.log(this.state.tick);
+        //         console.log(Date.now());
+        //         if (Date.now()-this.state.tick > 2000) {
+        //             console.log(this.state.tick);
+        //             //this.setState({tick: Date.now()});
+        //             this.moveBus(this.state.map, this.state.playerMarkers[0].marker, this.state.playerMarkers[0].player);
+        //         } 
+        // }
+
         return(
             <Map
             id="myMap"
@@ -41,9 +96,6 @@ class MapContainer extends React.Component<IMapContainerProps,IMapContainerState
                 let newMarkers = this.state.markers;
                  
                 const buildMarker = this.props.buildningStore.buildnings.map(buildning=>{
-                    // let infowindow = new (window as any).google.maps.InfoWindow({
-                    //     content: "Add your popup content here"
-                    //   });
 
                     let popup = undefined;
                     
@@ -56,14 +108,11 @@ class MapContainer extends React.Component<IMapContainerProps,IMapContainerState
                     });
 
                     (window as any).google.maps.event.addListener(marker, 'click', function() {
-                        //infowindow.open(map, marker);
-                        //this.hideAllInfoWindows(map);
 
                         newMarkers.forEach(function(m) {
                             if (m) {
                                 m.close(map, m);
                             }
-                            //marker.window.close(map, marker);
                             console.log(m);
                         });
 
@@ -72,58 +121,90 @@ class MapContainer extends React.Component<IMapContainerProps,IMapContainerState
                         let c = buildning.feed ? '<iframe src="//aka.ms/ampembed?url=' + buildning.feed + '" name="azuremediaplayer" scrolling="no" frameborder="no" align="center" height="280px" width="500px" allowfullscreen></iframe>' : '<div><p>No feed available!</p></div>'
 
                         popup = new (window as any).google.maps.InfoWindow({
-                            //content: '<iframe src="//aka.ms/ampembed?url=https%3A%2F%2Fspiderpigsms.blob.core.windows.net%2Fasset-216f0abd-9c30-4e80-b768-c3407940a70f%2FPowerPlant.mp4%3Fsv%3D2017-04-17%26sr%3Dc%26si%3D766ed076-30fd-4eb2-9904-04a3ea2a0eb3%26sig%3D1jjQaZUk5xCB07JH0uuGOCbYdkQf8lE9JjVXURI%252B5NI%253D%26st%3D2019-03-01T13%253A33%253A43Z%26se%3D2119-04-04T12%253A33%253A43Z" name="azuremediaplayer" scrolling="no" frameborder="no" align="center" height="280px" width="500px" allowfullscreen></iframe>'
                             content: c
-                            //content: '<iframe title="YouTube video player" class="youtube-player" type="text/html" width="480" height="390" src="https://www.youtube.com/embed/714-Ioa4XQw" frameborder="0"></iframe>'
                           })
-                          popup.open(map, marker);
-                        //window.location.href = marker.url;
-
+                        popup.open(map, marker);
                         newMarkers.push(popup);
-                        
                       });
-
-                    
-                    
                 });
                 
+
                 const buildMarkerPerson = this.props.playerStore.players.map(player=>{
-                    new (window as any).google.maps.Marker({
+                    let m = new (window as any).google.maps.Marker({
                         position:{lat:player.position.lat, lng:player.position.lng},
                         map:map,
                         title:player.title,
                         icon:player.icon
 
                     });
-                    
+                    //return m;
+                    return {
+                        marker: m,
+                        player: player
+                    }
                 });
-            //   var marker = new (window as any).google.maps.Marker({
-            //     position: { lat: 42.0974, lng: -72.5647 },
-            //     map: map,
-            //     title: 'test2',
-            //     icon:image
-            //   });
-            //   var marker2 = new (window as any).google.maps.Marker({
-            //     position: { lat: 42.0000, lng: -72.5000 },
-            //     map: map,
-            //     title: 'test'
-            //   });
+
+                this.setState({
+                    playerMarkers: buildMarkerPerson,
+                    map: map
+                })
+
+                // console.log(buildMarkerPerson);
+
+                // console.log(this.state.tick);
+                // console.log(Date.now());
+                // if (Date.now()-this.state.tick > 2000) {
+                //     console.log(this.state.tick);
+                //     //this.setState({tick: Date.now()});
+                //     this.moveBus(map, buildMarkerPerson[0].marker, buildMarkerPerson[0].player);
+                // } 
+
             }}
           />
         );
     }
 
-    //  hideAllInfoWindows(map: any) {
-    //     this.state.markers.forEach(function(marker) {
-    //     marker.infowindow.close(map, marker);
-    //     }); 
+    moveBus(map: any, marker: any, player: IPlayer) {
+        //console.log(marker);
+
+        let c = 0;
+        while (c < player.movementPattern.length) {
+            if (player.movementPattern[c].lat == player.position.lat && player.movementPattern[c].lng == player.position.lng) {
+                console.log("Found current pos: " + c);
+                break;
+            }
+            c++;
+        }
+
+        if (c < player.movementPattern.length-1) {
+            console.log("Moving next to " + (c+1) + " (max: " + (player.movementPattern.length-1) + ")");
+            marker.setPosition(player.movementPattern[c+1]);
+            player.position = player.movementPattern[c+1];
+        } else {
+            console.log("Initial pos");
+            marker.setPosition(player.movementPattern[0]);
+            player.position = player.movementPattern[0];
+        }
+
+        //let i = player.movementPattern.indexOf(player.position);
+        //console.log(player.movementPattern);
+        //console.log(player.position.lat);
+        //console.log(i);
+        // if (i < player.movementPattern.length-1) {
+        //     marker.setPosition(player.movementPattern[i+1]);
+        // } else {
+        //     marker.setPosition(player.movementPattern[0]);
+        // }
+        
+        
+    }
 }
+
 
 const enchanted = compose<IMapContainerProps,{}>(
  inject('buildningStore','playerStore'),
  observer
 );
-
 
 
 export default enchanted(MapContainer);
--- src/store/player/PlayerStore.ts
+++ src/store/player/PlayerStore.ts
@@ -9,14 +9,15 @@ import * as Smithers from '../../image/Waylon_Smithers.png';
 
 export interface IPlayerStoreProps{
     players:IObservableArray<IPlayer>;
-    movePosition:()=>void;
+    //movePosition:()=>void;
 }
 
-interface IPlayer{
+export interface IPlayer{
     id:number;
     position:IPosition;
     icon:any;
     title:string;
+    movementPattern:IPosition[]
 }
 
 interface IPosition{
@@ -26,26 +27,71 @@ interface IPosition{
 
 class PlayerStore implements IPlayerStoreProps{
     players = [
-    {title:'Homer Simpson',position:{lat:42.0954,lng:-72.5738}, icon:Homer_Simpson},
-    {title:'Bart Simpson',position:{lat:42.0906,lng:-72.5768}, icon:Bart_Simpson},
-    {title:'Maggie Simpson',position:{lat:42.0885,lng:-72.5706}, icon:Maggie_Simpson},
-    {title:'Marge Simpson',position:{lat:42.0955,lng:-72.5849}, icon:Marge},
-    {title:'Lisa Simpson',position:{lat:42.0945,lng:-72.5812}, icon:Lisa_Simpson},
-    {title:'Spiderpig',position:{lat:42.0920,lng:-72.5700},icon:Spiderpig},
-    {title:'Waylon Smithers',position:{lat:42.0920,lng:-72.5650},icon:Smithers}
+    {
+        title:'Homer Simpson',
+        position:{lat:42.0954,lng:-72.5738},
+        icon:Homer_Simpson,
+        movementPattern : [
+            {lat:42.0954,lng:-72.5738},
+            {lat:42.0960,lng:-72.5800}
+        ]
+    },
+    {
+        title:'Bart Simpson',position:{lat:42.0906,lng:-72.5768}, icon:Bart_Simpson,
+        movementPattern : [
+            {lat:42.0906,lng:-72.5768},
+            {lat:42.0920,lng:-72.5800}
+        ]
+
+    },
+    {
+        title:'Maggie Simpson',position:{lat:42.0885,lng:-72.5706}, icon:Maggie_Simpson,
+        movementPattern : [
+            {lat:42.0885,lng:-72.5706},
+            {lat:42.0885,lng:-72.5706}
+        ]
+    },
+    {
+        title:'Marge Simpson',position:{lat:42.0955,lng:-72.5849}, icon:Marge,
+        movementPattern : [
+            {lat:42.0955,lng:-72.5849},
+            {lat:42.0955,lng:-72.5849}
+        ]
+    },
+    {
+        title:'Lisa Simpson',position:{lat:42.0945,lng:-72.5812}, icon:Lisa_Simpson,
+        movementPattern : [
+            {lat:42.0945,lng:-72.5812},
+            {lat:42.0945,lng:-72.5812}
+        ]
+    },
+    {
+        title:'Spiderpig',position:{lat:42.0920,lng:-72.5700},icon:Spiderpig,
+        movementPattern : [
+            {lat:42.0920,lng:-72.5700},
+            {lat:42.0920,lng:-72.5700}
+        ]
+    },
+    {
+        title:'Waylon Smithers',position:{lat:42.0920,lng:-72.5650},icon:Smithers,
+        movementPattern : [
+            {lat:42.0920,lng:-72.5650},
+            {lat:42.0920,lng:-72.5650}
+        ]
+    }
 ] as any;
 
-    movePosition=()=>{
-        const blat =42.0974;
-        const blng =-72.5647;
-        const findPlayer:IPlayer = this.players.find((player:IPlayer)=> player.id ===0);
-        console.log(findPlayer.title);
-    }
+    // movePosition=()=>{
+    //     const blat =42.0974;
+    //     const blng =-72.5647;
+    //     const findPlayer:IPlayer = this.players.find((player:IPlayer)=> player.id ===0);
+    //     console.log(findPlayer.title);
+    // }
 }
 
 decorate<PlayerStore>(PlayerStore,{
     players:observable,
-    movePosition:action('Move player')
+    //movePosition:action('Move player')
 });
 
 const buildning = new PlayerStore();
