c214371ee677f6416c26b0e0803dd2a7ea6f5419
Ragnhild Baumann <rb@infoworker.no>
2019-03-01 22:31:45 +0100

    lots of stuff

--- src/store/player/PlayerStore.ts
+++ src/store/player/PlayerStore.ts
@@ -6,6 +6,11 @@ import * as Marge from '../../image/Marge.png';
 import * as Maggie_Simpson from '../../image/Maggie_Simpson.png';
 import * as Spiderpig from '../../image/spiderpig.png';
 import * as Smithers from '../../image/Waylon_Smithers.png';
+import * as chris from '../../image/chris_burns_orig.png';
+import * as seb from '../../image/seb_troy.png';
+import * as marius from '../../image/marius_edna.png';
+import * as laura from '../../image/marge_laura.png';
+import * as erwin from '../../image/erwin_flanders.png';
 
 export interface IPlayerStoreProps{
     players:IObservableArray<IPlayer>;
@@ -32,7 +37,12 @@ class PlayerStore implements IPlayerStoreProps{
     {title:'Marge Simpson',position:{lat:42.0955,lng:-72.5849}, icon:Marge},
     {title:'Lisa Simpson',position:{lat:42.0945,lng:-72.5812}, icon:Lisa_Simpson},
     {title:'Spiderpig',position:{lat:42.0920,lng:-72.5700},icon:Spiderpig},
-    {title:'Waylon Smithers',position:{lat:42.0920,lng:-72.5650},icon:Smithers}
+    {title:'Waylon Smithers',position:{lat:42.0920,lng:-72.5650},icon:Smithers},
+    {title:'Seb Troy',position:{lat:42.0974,lng:-72.5657},icon:seb},
+    {title:'Chris Burns',position:{lat:42.0920,lng:-72.5620},icon:chris},
+    {title:'Marius Edna',position:{lat:42.0940,lng:-72.5695},icon:marius},
+    {title:'Marge Laura',position:{lat:42.0930,lng:-72.5785},icon:laura},
+    {title:'Erwin Flanders',position:{lat:42.0940,lng:-72.5950},icon:erwin},
 ] as any;
 
     movePosition=()=>{
